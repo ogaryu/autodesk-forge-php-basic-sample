b549364fbd550f88b8cf4de8d32cfb17a5fbba02
Ryuji <ryuji.ogasawara@autodesk.com>
2016-10-19 01:21:18 +0900

    change jquery to non-minified

--- application/views/layout.php
+++ application/views/layout.php
@@ -6,7 +6,7 @@
     <title><?php if (isset($title)): echo $this->escape($title) . ' - '; endif; ?>Autodesk Forge PHP Basic Sample</title>
     
     <!-- jQuery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
 
     <!-- Bootstrap -->
     <!-- Latest compiled and minified CSS -->
